5ee09051590a7f168ef60b51ff92cf04d8627dbc
LiuXiao <liuxiao20050617@163.com>
2025-03-13 23:20:57 +0800

    toString of PBT

--- hw/hw03.lean
+++ hw/hw03.lean
@@ -6,6 +6,30 @@ inductive PBT : Type
 | p_node : String → PBT
 -- Induction steps
 | p_family: PBT → PBT → PBT → PBT
--- left Node + right node + parent node = PBT
+-- parent node + left Node + right node  = PBT
 
 open PBT
+
+def PBT_to_String : PBT → String
+  -- Base case
+  | p_node s => s
+  -- Induction steps
+  | p_family p l r => PBT_to_String p ++ " "++ PBT_to_String l ++ " ^ " ++ PBT_to_String r ++ " ^ "
+
+instance : ToString PBT where
+  toString := PBT_to_String
+
+def A : PBT := p_node "A"
+def B : PBT := p_node "B"
+def C : PBT := p_node "C"
+def D : PBT := p_node "D"
+def E : PBT := p_node "E"
+def F : PBT := p_node "F"
+def G : PBT := p_node "G"
+def H : PBT := p_node "H"
+def I : PBT := p_node "I"
+
+def ex_PBT : PBT :=
+  p_family A (p_family B D (p_family E H I)) (p_family C F G)
+
+#eval toString ex_PBT
